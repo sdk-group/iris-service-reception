3381ac05f9bb0460ed84f56154db3ab41376bf14
artificial-life <proteus.protocol@gmail.com>
2016-05-31 21:07:12 +0600

    upd

--- src/reception/reception.js
+++ src/reception/reception.js
@@ -5,32 +5,29 @@ let emitter = require("global-queue");
 let table_template = require('./service-info-template.js');
 
 class Reception {
-	constructor() {
-		this.emitter = emitter;
-	}
-	init(config) {
+  constructor() {
+    this.emitter = emitter;
+  }
+  init(config) {
 
-	}
-	launch() {
-		return Promise.resolve(true);
-	}
-	actionServiceInfo(params) {
-		let now = moment().format();
+  }
+  launch() {
+    return Promise.resolve(true);
+  }
+  actionServiceInfo(params) {
+    let now = moment().format();
 
-		table_template.interval = [now, now];
-		table_template.department = params.department;
-		console.log(table_template);
-		return this.emitter.addTask('reports', {
-			_action: 'get-table',
-			table: table_template
-		}).then(d => {
-			console.log(d);
-			return d;
-		});
-	}
-	actionWorkstationInfo() {
+    table_template.interval = [now, now];
+    table_template.department = params.department;
 
-	}
+    return this.emitter.addTask('reports', {
+      _action: 'get-table',
+      table: table_template
+    });
+  }
+  actionWorkstationInfo() {
+
+  }
 }
 
 module.exports = Reception;
