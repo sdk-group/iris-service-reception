87e208ab84c0ed6834066cfe5b86cbaf55ae7928
artificial-life <proteus.protocol@gmail.com>
2016-06-28 21:51:07 +0600

    patchwerk

--- src/reception/reception.js
+++ src/reception/reception.js
@@ -1,33 +1,32 @@
 'use strict'
 
 let emitter = require("global-queue");
+let patchwerk = require('patchwerk')(emitter);
 
 let table_template = require('./service-info-template.js');
 
 class Reception {
-  constructor() {
-    this.emitter = emitter;
-  }
-  init(config) {
+	constructor() {
+		this.emitter = emitter;
+	}
+	init(config) {}
+	launch() {
+		return Promise.resolve(true);
+	}
+	actionServiceInfo(params) {
+		let now = moment().format();
 
-  }
-  launch() {
-    return Promise.resolve(true);
-  }
-  actionServiceInfo(params) {
-    let now = moment().format();
+		table_template.interval = [now, now];
+		table_template.department = params.department;
 
-    table_template.interval = [now, now];
-    table_template.department = params.department;
+		return this.emitter.addTask('reports', {
+			_action: 'get-table',
+			table: table_template
+		});
+	}
+	actionWorkstationInfo() {
 
-    return this.emitter.addTask('reports', {
-      _action: 'get-table',
-      table: table_template
-    });
-  }
-  actionWorkstationInfo() {
-
-  }
+	}
 }
 
 module.exports = Reception;
