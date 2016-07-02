6e61b84db29a67797de2d9e72a5eca9f0c5dae67
artificial-life <proteus.protocol@gmail.com>
2016-07-02 17:30:33 +0600

    fix

--- src/reception/reception.js
+++ src/reception/reception.js
@@ -7,49 +7,50 @@ let table_template = require('./service-info-template.js');
 let active_tickets = require('./active-tickets-template.js');
 
 class Reception {
-  constructor() {
-    this.emitter = emitter;
-  }
-  init(config) {}
-  launch() {
-    return Promise.resolve(true);
-  }
-  getTodayStats(params, template) {
-    let now = moment().format();
+	constructor() {
+		this.emitter = emitter;
+	}
+	init(config) {}
+	launch() {
+		return Promise.resolve(true);
+	}
+	getTodayStats(params, template) {
+		let now = moment().format();
 
-    table_template.interval = [now, now];
-    table_template.department = params.department;
+		template.interval = [now, now];
+		template.department = params.department;
 
-    return this.emitter.addTask('reports', {
-      _action: 'get-table',
-      table: template
-    });
+		return this.emitter.addTask('reports', {
+			_action: 'get-table',
+			table: template
+		});
 
-  }
-  actionServiceInfo(params) {
-    return this.getTodayStats(params, table_template)
-  }
-  actionWorkstationInfo(params) {
+	}
+	actionServiceInfo(params) {
+		return this.getTodayStats(params, table_template)
+	}
+	actionWorkstationInfo(params) {
 
-    let requests = {
-      active_tickets: this.getTodayStats(params, active_tickets),
-      workstations: patchwerk.get('Workstation', {
-        department: params.department
-      })
-    };
+		let requests = {
+			active_tickets: this.getTodayStats(params, active_tickets),
+			workstations: patchwerk.get('Workstation', {
+				department: params.department,
+				counter: '*'
+			})
+		};
 
-    return Promise.props(requests).then(data => {
-      let workstations = data.workstations;
+		return Promise.props(requests).then(data => {
+			let workstations = data.workstations;
 
-      _.forEach(data.active_tickets, ticket => {
-        let dest = ticket.destination;
-        let owner = _.find(workstations, ['id', dest]);
-        owner.current_ticket = ticket;
-      });
+			_.forEach(data.active_tickets, ticket => {
+				let dest = ticket.destination;
+				let owner = _.find(workstations, ['id', dest]);
+				owner.current_ticket = ticket;
+			});
 
-      return workstations;
-    });
-  }
+			return workstations;
+		});
+	}
 }
 
 module.exports = Reception;
